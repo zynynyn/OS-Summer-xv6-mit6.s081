875b4f28e13672969c9cc637438e0fd384c09948
zynynyn <yuning24@126.com>
2024-08-03 01:26:55 +0800

    update cow

--- kernel/proc.h
+++ kernel/proc.h
@@ -85,7 +85,7 @@ enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 // Per-process state
 struct proc {
   struct spinlock lock;
-
+  
   // p->lock must be held when using these:
   enum procstate state;        // Process state
   void *chan;                  // If non-zero, sleeping on chan
