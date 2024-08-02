2ec983bf4bc456a900000a9479de737833f3fc92
zynynyn <yuning24@126.com>
2024-08-03 01:28:55 +0800

    update cow

--- user/uthread.c
+++ user/uthread.c
@@ -10,14 +10,33 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context ctx; // 在 thread 中添加 context 结构体
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context* old, struct context* new);
               
 void 
 thread_init(void)
@@ -58,10 +77,7 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    thread_switch(&t->ctx, &next_thread->ctx); // 切换线程
   } else
     next_thread = 0;
 }
@@ -75,7 +91,10 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->ctx.ra = (uint64)func;       // 返回地址
+  // thread_switch 的结尾会返回到 ra，从而运行线程代码
+  t->ctx.sp = (uint64)&t->stack + (STACK_SIZE - 1);  // 栈指针
+  // 将线程的栈指针指向其独立的栈，将 sp 设置为指向 stack 的最高地址
 }
 
 void 
